277c46d1eb4ab938f861ac247445f53eead7a204
Shelbot77 <alessiodeangelis021@gmail.com>
2022-12-28 10:23:58 +0100

    implementazione funzione addnewmessage

--- snack3/script.js
+++ snack3/script.js
@@ -3,15 +3,52 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-        
-      
+      myInput: '',
+      messages: [
+        {
+          text: 'ciao',
+          status: 'sent',
+        },
+        {
+          text: 'come stai?',
+          status: 'sent',
+
+        },
+        {
+          text: 'ciao, bene grazie',
+          status: 'received',
+
+        }
+      ]
+
+
     }
   },
-  methods:{
-    
+  methods: {
+    addNewMessage(content) {
+      messageObj = {
+        text: this.myInput,
+        status: 'sent',
+      },
+        this.messages.push(messageObj);
+      this.myInput = '';
+
+    }
+
   },
-  created(){
-    
-    
+  created() {
+
+
   }
 }).mount('#app')
+
+/* /** Creare un input che permetta all'utente di inserire un messaggio e aggiungerlo ad una conversazione (tipo whatsapp).
+ Dopo averlo aggiunto chiedere all'API una frase random (attraverso random/sentence) aggiungendo anch'essa al thread,
+ differenziando i messaggi utente da quelli del computer. */
+
+
+/* axios.get('/user?ID=12345')
+ .then(function (response) {
+   // handle success
+   console.log(response);
+ }) */
